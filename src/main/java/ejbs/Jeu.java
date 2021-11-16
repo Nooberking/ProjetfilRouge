c4bf2508c6586f4273370821e80454143f5fb01f
FlorianReecht <re.florian@hotmail.fr>
2021-11-16 10:10:24 +0100

    ajout fonction combat

--- src/main/java/ejbs/Jeu.java
+++ src/main/java/ejbs/Jeu.java
@@ -1,5 +1,20 @@
 package ejbs;
 
-public class Jeu {
+public class Jeu 
+{
+
+	
+	public void Combat(Entity e1,Entity e2)
+	{
+		System.out.println(e1.attack(e2));
+		System.out.println(e2.attack(e1));
+	}
+	public static void main(String[] args) {
+		
+		Personnage joueur=Personnage.createWarrior();
+		Ennemy firstenemy= new Ennemy();
+		Jeu j1= new Jeu();
+		j1.Combat(joueur, firstenemy);
+	}
 
 }
